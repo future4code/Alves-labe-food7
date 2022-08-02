76a41baf923dc4bb0b86e885d39ddbb797043a99
gabrielwenchenck <wenck13@gmail.com>
2022-08-02 12:32:29 -0300

    hooks adress

--- rappi4/src/hooks/useUnprotectedPage.js
+++ rappi4/src/hooks/useUnprotectedPage.js
@@ -2,13 +2,13 @@ import { goToAdress } from "../routes/cordinator";
 import { useNavigate } from "react-router-dom";
 import { useLayoutEffect } from "react";
 
-const useUntectedPage = () => {
+const useUnprotectedPage = () => {
   const navigate = useNavigate();
   useLayoutEffect(() => {
     const token = localStorage.getItem("token");
-    if (token) {      
+    if (token) {
       goToAdress(navigate);
     }
   }, [navigate]);
 };
-export default useUntectedPage
+export default useUnprotectedPage;
