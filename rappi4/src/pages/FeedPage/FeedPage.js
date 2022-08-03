8657291252fbeee882ba1f54aedb22a4a7c04a0c
Raiza gomes da silva <gomesraiza224@gmail.com>
2022-08-02 23:13:33 -0300

    lista feed

--- rappi4/src/pages/FeedPage/FeedPage.js
+++ rappi4/src/pages/FeedPage/FeedPage.js
@@ -1,16 +1,55 @@
-import React , {useContext}from "react";
-import { GlobalContext } from "../../services/Global/GlobalContext";
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+//import { GlobalContext } from "../../services/Global/GlobalContext";
+//import { useNavigate } from 'react-router-dom'
 import { BASE_URL } from "../../constants/urls";
+import axios from "axios"
+import { Image } from "./styled";
 
 
-const FeedPage = ()=> {
- 
- // const {restaurants,setRestaurantes}= useContext(GlobalContext)
- // console.log(setRestaurantes)
+
+const FeedPage = () => {
+
+  const [restaurants, setRestaurantes] = useState([])
+
+  useEffect(() => {
+    PegarRestaurantes()
+  }, [])
+
+
+  const PegarRestaurantes = () => {
+    const url = `${BASE_URL}/restaurants`
+    const token = localStorage.getItem("token")
+    const header = {
+      headers: {
+        auth: token
+      },
+    }
+    axios.get(url, header)
+      .then((resp) => {
+        setRestaurantes(resp.data.restaurants)
+        console.log(resp.data.restaurants)
+      })
+      .catch((err) => {
+        console.log("errooooo", err.response)
+      })
+  }
+
+  const rendeRestaurants = restaurants.map((res) => {
+    return <div key={res.id}>
+
+      <div>
+        <Image src={res.logoUrl} />
+      </div>
+      <p>{res.name}</p>
+      <p>{res.deliveryTime}min</p>
+      <p>Frete R${res.shipping},00</p>
+    </div>
+  })
 
   return (
-    <div>feed</div>
+    <div>
+      {rendeRestaurants}
+    </div>
   )
 }
 
---
+++ rappi4/src/pages/FeedPage/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+
+export const Image = styled.img`
+ width: 328px;
+ height: 120px;
+ margin: 0 0 12px;
+ object-fit: contain;
+`
+
+export const CardRestaurante = styled.div`
+
+  width: 360px;
+  height: 196px;
+  margin: 50px 0 0;
+  padding: 8px 16px 0;
+
+`
+
+
+   
+ 
