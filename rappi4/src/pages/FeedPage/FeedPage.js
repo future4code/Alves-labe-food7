adb8d0ca4afc39c5a4debaec607707eef1f25bcb
AndreEletherio <andremachadoeletherio@gmail.com>
2022-08-05 08:22:33 -0300

    Mudanças no feed

--- rappi4/src/pages/FeedPage/FeedPage.js
+++ rappi4/src/pages/FeedPage/FeedPage.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../../constants/urls";
 import axios from "axios";
 import { goToDetailRestaurant } from "../../routes/cordinator";
-import { CardRestaurante, Image, Line, ResName, Screen, Main } from "./styled";
+import { CardRestaurante, Image, Line, ResName, Screen, Main, Loading } from "./styled";
 import { HeaderWord } from "../../components/HeaderWord";
 import { Input } from "../SearchPage/styles/styled";
 import { Footer } from '../../components/Footer/Footer'
@@ -14,6 +14,7 @@ import { goToSearch } from "../../routes/cordinator";
 const FeedPage = () => {
   const [restaurants, setRestaurantes] = useState([]);
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     PegarRestaurantes();
@@ -30,7 +31,8 @@ const FeedPage = () => {
     axios
       .get(url, header)
       .then((resp) => {
-        setRestaurantes(resp.data.restaurants);        
+        setRestaurantes(resp.data.restaurants);
+        setIsLoading(false)     
       })
       .catch((err) => {
         console.log("errooooo", err.response);
@@ -61,7 +63,7 @@ const FeedPage = () => {
       <HeaderWord word={"Rappi4"} />
       <Main>
         <Input placeholder="Restaurante" onClick={()=>goToSearch(navigate)} />
-        {rendeRestaurants}
+        {isLoading ? <Loading>Carregando...</Loading>  : rendeRestaurants}
       </Main>
       <Footer page="home" />
     </Screen>
